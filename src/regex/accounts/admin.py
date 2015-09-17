655f7d13899d2fc769f00dfd24477ca5fe6610ec
Sergei Maertens <sergei@maykinmedia.nl>
2015-09-17 22:11:13 +0200

    Register user model in admin

---
+++ src/regex/accounts/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    ordering = ('email',)
+    search_fields = ('email', 'first_name', 'last_name')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
