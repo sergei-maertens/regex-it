2c8131cc51c9a0f222b1c1d7697ac719ab0e4db8
Sergei Maertens <sergei@maykinmedia.nl>
2015-10-20 23:09:44 +0200

    More deployment fixes

--- src/wsgi.py
+++ src/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+
+def setupenv():
+    src_dir = os.path.dirname(os.path.abspath(__file__))
+    sys.path = [src_dir] + sys.path
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "regex.conf.settings")
 
+setupenv()
 application = get_wsgi_application()
