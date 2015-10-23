a83a06cb0bbe2cf7873c3eb3996a0731400095ee
Sergei Maertens <sergei@maykinmedia.nl>
2015-10-24 00:42:49 +0200

    Tricky path config

--- src/regex/conf/settings/base.py
+++ src/regex/conf/settings/base.py
@@ -87,7 +87,7 @@ PRIVATE_MEDIA_URL = '/protected/'
 
 SENDFILE_BACKEND = 'sendfile.backends.nginx'
 SENDFILE_ROOT = PRIVATE_MEDIA_ROOT
-SENDFILE_URL = PRIVATE_MEDIA_URL
+SENDFILE_URL = PRIVATE_MEDIA_URL[:-1]
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = os.environ.get('SECRET_KEY') or '2tb%e$$k)0+9zv5d!#_m_c4#x1tkk7)va+o0&#d2=pokzf287='
