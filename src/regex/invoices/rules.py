b739d9e9bf966fa7270f63503a9c20885eafb001
Sergei Maertens <sergei@maykinmedia.nl>
2015-10-24 00:12:45 +0200

    initial implementation for permission checks

--- src/regex/invoices/rules.py
+++ src/regex/invoices/rules.py
@@ -2,7 +2,7 @@ import rules
 
 
 def is_invoice_contact(user, invoice):
-    import bpdb; bpdb.set_trace()
+    return invoice.client.contacts.filter(user__id=user.id).exists()
 
 
 rules.add_perm('invoices.can_download_pdf', is_invoice_contact)
