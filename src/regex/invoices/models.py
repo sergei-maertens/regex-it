278cf226ad8ed37abbaacecb80acb5b91375a842
Sergei Maertens <sergei@maykinmedia.nl>
2015-10-29 20:02:08 +0100

    Implement reverse-charge VAT on invoice detail

--- src/regex/invoices/models.py
+++ src/regex/invoices/models.py
@@ -8,6 +8,7 @@ from django.db import models, transaction
 from django.db.models import F, Sum, Max
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.utils import timezone
+from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
 
 from regex.crm.models import TaxRates
@@ -135,7 +136,7 @@ class Invoice(models.Model):
         totals = self.get_totals()
         return totals['base__sum'] + totals['tax__sum']
 
-    @property
+    @cached_property
     def vat_reverse_charge(self):
         return self.client.country != settings.SITE_COUNTRY
 
