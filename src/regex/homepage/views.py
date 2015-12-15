fb536cc3626c79c0950b86d8b8e956fb261b5cb0
Sergei Maertens <sergeimaertens@gmail.com>
2015-12-15 09:19:00 +0100

    Initial contact page set up

--- src/regex/homepage/views.py
+++ src/regex/homepage/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomepageView(TemplateView):
     template_name = 'homepage/base.html'
+
+
+class ContactView(TemplateView):
+    template_name = 'homepage/contact.html'
