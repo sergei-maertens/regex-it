fd15e714393565b69205751b6ca350c2714ce564
Sergei Maertens <sergei@maykinmedia.nl>
2017-09-28 02:22:56 +0200

    Show some aggregated data

--- src/regex/work_entries/admin.py
+++ src/regex/work_entries/admin.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from django.contrib import admin
+from django.db.models import F, Sum
 
 from import_export.admin import ImportExportActionModelAdmin
 
@@ -10,3 +13,16 @@ class WorkEntryAdmin(ImportExportActionModelAdmin):
     list_display = ('start', 'end', 'project', 'notes')
     list_filter = ('project__client', 'project', 'start')
     search_fields = ('notes',)
+    change_list_template = 'admin/work_entries/workentry/change_list.html'
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(request, extra_context=None)
+        cl = response.context_data['cl']
+        queryset = cl.get_queryset(request)
+        duration = (
+            queryset
+            .annotate(duration=F('end') - F('start'))
+            .aggregate(Sum('duration'))['duration__sum']
+        ) or timedelta()
+        response.context_data['total_duration'] = duration.total_seconds() / 3600
+        return response
