98107fc29d3a37434e6d7dd45496db21767f8db7
Sergei Maertens <sergei@maykinmedia.nl>
2023-10-09 22:31:54 +0200

    :sparkles: Implement generating ZIP file with administration

--- src/regex/administration/forms.py
+++ src/regex/administration/forms.py
@@ -1,5 +1,6 @@
 import zipfile
 from dataclasses import dataclass
+from pathlib import Path
 from typing import BinaryIO
 
 from django import forms
@@ -9,10 +10,13 @@ from django.utils.translation import gettext_lazy as _
 from regex.expenses.models import Invoice as DebitInvoice
 from regex.invoices.models import Invoice as CreditInvoice
 
+from .utils import DateRange, get_current_quarter, get_previous_quarter
+
 
 class ExportQuarterChoices(models.TextChoices):
     previous = "previous", _("Previous")
     current = "current", _("Current")
+    none = "", _("None")
 
 
 @dataclass
@@ -23,7 +27,14 @@ class ExportData:
     def write_zip(self, outfile: BinaryIO):
         with zipfile.ZipFile(outfile, "w") as archive:
             archive.mkdir("credit")
+            for invoice in self.credit:
+                pdf_path = Path(invoice.pdf.path)
+                archive.write(pdf_path, Path("credit") / pdf_path.name)
+
             archive.mkdir("debit")
+            for invoice in self.debit:
+                pdf_path = Path(invoice.pdf.path)
+                archive.write(pdf_path, Path("debit") / pdf_path.name)
 
 
 class ExportAdministrationForm(forms.Form):
@@ -44,8 +55,40 @@ class ExportAdministrationForm(forms.Form):
         required=False,
     )
 
+    def clean(self):
+        quarter = self.cleaned_data.get("quarter")
+        start = self.cleaned_data.get("start")
+        end = self.cleaned_data.get("end")
+
+        if not quarter or quarter == "none":
+            if not start or not end:
+                raise forms.ValidationError(
+                    _("Specify a quarter or start and end date")
+                )
+
+        match quarter:
+            case "previous":
+                date_range = get_previous_quarter()
+            case "current":
+                date_range = get_current_quarter()
+            case "none":
+                date_range = DateRange(start=start, end=end)
+
+        self.cleaned_data["date_range"] = date_range
+
+        return self.cleaned_data
+
     def prepare_export_date(self) -> ExportData:
+        date_range = self.cleaned_data["date_range"]
+        debit = DebitInvoice.objects.filter(
+            date__gte=date_range.start,
+            date__lte=date_range.end,
+        )
+        credit = CreditInvoice.objects.filter(
+            date__gte=date_range.start,
+            date__lte=date_range.end,
+        )
         return ExportData(
-            credit=[],
-            debit=[],
+            credit=list(credit),
+            debit=list(debit),
         )
