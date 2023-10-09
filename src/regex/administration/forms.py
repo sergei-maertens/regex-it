6d8e4ed2799b1176a6414439df86c6c048ff782c
Sergei Maertens <sergei@maykinmedia.nl>
2023-10-09 22:17:58 +0200

    :sparkles: Wire up file export/download view for ZIP file

--- src/regex/administration/forms.py
+++ src/regex/administration/forms.py
@@ -1,5 +1,51 @@
+import zipfile
+from dataclasses import dataclass
+from typing import BinaryIO
+
 from django import forms
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from regex.expenses.models import Invoice as DebitInvoice
+from regex.invoices.models import Invoice as CreditInvoice
+
+
+class ExportQuarterChoices(models.TextChoices):
+    previous = "previous", _("Previous")
+    current = "current", _("Current")
+
+
+@dataclass
+class ExportData:
+    credit: list[CreditInvoice]
+    debit: list[DebitInvoice]
+
+    def write_zip(self, outfile: BinaryIO):
+        with zipfile.ZipFile(outfile, "w") as archive:
+            archive.mkdir("credit")
+            archive.mkdir("debit")
 
 
 class ExportAdministrationForm(forms.Form):
-    pass
+    quarter = forms.ChoiceField(
+        label=_("Quarter"),
+        help_text=_("Which quarter to export."),
+        choices=lambda: ExportQuarterChoices.choices,
+        required=False,
+    )
+    start = forms.DateField(
+        label=_("Start"),
+        help_text=_("Export from this date."),
+        required=False,
+    )
+    end = forms.DateField(
+        label=_("End"),
+        help_text=_("Export until (including) this date."),
+        required=False,
+    )
+
+    def prepare_export_date(self) -> ExportData:
+        return ExportData(
+            credit=[],
+            debit=[],
+        )
--- src/regex/administration/views.py
+++ src/regex/administration/views.py
@@ -1,5 +1,7 @@
+from io import BytesIO
+
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.http import HttpRequest
+from django.http import FileResponse, HttpRequest
 from django.views.generic import FormView
 
 from .forms import ExportAdministrationForm
@@ -15,3 +17,18 @@ class SuperUserRequired(UserPassesTestMixin):
 class ExportAdministrationView(SuperUserRequired, FormView):
     form_class = ExportAdministrationForm
     template_name = "administration/export_administration.html"
+
+    def form_valid(self, form: ExportAdministrationForm):
+        export_data = form.prepare_export_date()
+
+        response = FileResponse(
+            as_attachment=True,
+            filename="administration.zip",
+            content_type="application/zip",
+        )
+        outfile = BytesIO()
+        export_data.write_zip(outfile)
+        outfile.seek(0)
+        response.streaming_content = outfile
+
+        return response
